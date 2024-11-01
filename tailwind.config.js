18e2bbdee2dbb1f9b0f0bae6508d6b5abfabeb94
Abdulrahman <110260275+Omotayo21@users.noreply.github.com>
2024-11-01 23:56:30 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./src todo list/**/*.{js,ts,jsx,tsx}",
-    "./src multi/**/*.{js,ts,jsx,tsx}",
    
   ],
   theme: {
     screens :{
-      'sm' :'128px',
-      'md' :'768px',
-      'lg' :'1024px',
-      'xl' :'1280px',
+      sm: "320px",
+      md: "768px",
+      lg: "1024px",
     },
     extend: {},
   },
